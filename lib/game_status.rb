1c7eda4da78420b5268d5f4eebe0af65fecb6fad
William Damm <wdamm@powerhrg.com>
2020-04-11 00:39:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,40 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def won?(board) 
+ win_combo(board).first
+end
+
+def win_combo(board)
+  WIN_COMBINATIONS.select do |winner|
+    (board[winner[0]] =="X" && board[winner[1]] == "X" && board[winner[2]] == "X") || (board[winner[0]] =="O" && board[winner[1]] == "O" && board[winner[2]] == "O")
+  end
+end
+
+def full?(board)
+board.none? {|i|
+i == " "
+}
+end
+
+def draw?(board)
+full?(board) && !won?(board)
+end
+
+def over?(board)
+ full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+if won?(board) != false
+  array = won?(board)
+    space = array[0]
+    win_sig = board[space]
+    win_sig
+  else
+    nil
+end
+end
+
